2ebef1a83db7de4366cc088ab6bf362835da8a06
truongsinistar <truong@sinistar.ca>
2021-07-08 13:39:49 -0400

    lesson8 - realtime listener

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ const renderCafe = (doc) => {
 
 // Getting Data and Ordering Data
 
-db.collection("cafes").orderBy('name').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    renderCafe(doc)
-  })
-})
+// db.collection("cafes").orderBy('name').get().then((snapshot) => {
+//   snapshot.docs.forEach(doc => {
+//     renderCafe(doc)
+//   })
+// })
 
 
 // Saving Data
@@ -64,4 +64,18 @@ form.addEventListener('submit', (e) => {
   })
   form.name.value = '';
   form.city.value = ''
+})
+
+
+// Real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if(change.type == 'added') {
+      renderCafe(change.doc);
+    } else if (change.type == 'removed') {
+      let li = cafeList.querySelector(`[data-id=${change.doc.id}]`)
+      cafeList.removeChild(li)
+    }
+  })
 })
